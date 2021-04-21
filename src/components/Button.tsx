4843a1a6bd2b199f03954237b1967f7c43fe33f9
amorimmaria <mariadasgracasdiasamorim@gmail.com>
2021-04-21 14:33:11 -0300

    Aula 02 finalizada

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,40 +1,39 @@
 import React from 'react'
 import { TouchableOpacity, Text, StyleSheet, TouchableOpacityProps } from 'react-native'
-import colors from '../styles/colors'
 
-interface ButttonProps extends TouchableOpacityProps {
+import colors from '../styles/colors'
+import fonts from '../styles/fonts'
+
+interface ButtonProps extends TouchableOpacityProps {
   title: string;
 }
 
-export function Button({ title, ...rest } : ButttonProps){
-  return(
+export function Button({ title, ...rest }: ButtonProps) {
+  return (
     <TouchableOpacity 
-      style={styles.button} 
-      activeOpacity={0.7}
+      style={styles.container}
       {...rest}
     >
-      <Text style={styles.buttonText}>
-         {title}
+      <Text style={styles.text}>
+        { title }
       </Text>
     </TouchableOpacity>
-
   )
 }
 
 const styles = StyleSheet.create({
-  
-  button: {
+  container: {
     backgroundColor: colors.green,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 16,
-    marginBottom: 10,
     height: 56,
-    width: 56
+    borderRadius: 16,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  buttonText: {
+  text: {
+    fontSize: 16,
     color: colors.white,
-    fontSize: 24
+    fontFamily: fonts.heading
+
   }
 
 })
